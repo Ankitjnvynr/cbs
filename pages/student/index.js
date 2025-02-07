c1a23e48f64bd100412bf9f81bf2a67ef4daa361
ankitjnvynr <ankitbkana@outlook.com>
2025-02-07 10:42:39 +0530

    adding frontend

--- pages/student/index.js
+++ pages/student/index.js
@@ -1,42 +1,29 @@
-import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import Logo from '/public/images/cbslogo.png';
-import Phone from '/public/images/telephone.svg';
-import Image from 'next/image';
-import hero1 from '/public/images/slider/slider-1abc.jpg';
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Logo from "/public/images/cbslogo.png";
+import Phone from "/public/images/telephone.svg";
+import Scrollbar from "../../components/scrollbar/scrollbar";
+
+import Footer from "../../components/footer/Footer";
+import { Signup } from "../../components/studentDashboard/Signup";
 
 export default function Index() {
-    // Custom styles
-    const backgroundStyle = {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100vh',
-        zIndex: -1,
-        objectFit: 'cover',
-    };
+  return (
+    <>
+      <Navbar Logo={Logo} hclass={"wpo-site-header s1"} telephone={Phone} />
+      <div
+        style={{
+          minHeight: "70vh",
+          padding:'20px',
+          paddingTop:'150px',
+        }}
+      >
+        
+      <Signup/>
+      </div>
 
-    const overlayStyle = {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100vh',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', // 50% black overlay
-        zIndex: -1,
-    };
-
-    return (
-        <>
-            {/* Background Image */}
-            <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
-                <Image src={hero1} alt="Background" layout="fill" objectFit="cover" style={backgroundStyle} />
-                <div style={overlayStyle}></div>
-            </div>
-
-            {/* Navbar */}
-            <Navbar Logo={Logo} hclass={"wpo-site-header s3"} telephone={Phone} />
-        </>
-    );
+      <Footer />
+      <Scrollbar />
+    </>
+  );
 }
