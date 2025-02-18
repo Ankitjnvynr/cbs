60c448babd01a5f9b2ca49cd83cf748bb12030b0
ankitjnvynr <ankitbkana@outlook.com>
2025-02-18 15:59:53 +0530

    dfd

---
+++ api/blogs.js
@@ -0,0 +1,75 @@
+// images
+import blogImg1 from "/public/images/blog/result-degree.jpg";
+import blogImg2 from "/public/images/blog/result-degree.jpg";
+import blogImg3 from "/public/images/blog/result-degree.jpg";
+
+import recent1 from "/public/images/recent-posts/img-1.jpg";
+import recent2 from "/public/images/recent-posts/img-2.jpg";
+import recent3 from "/public/images/recent-posts/img-3.jpg";
+
+
+import blogSingleImg1 from "/public/images/blog/1.jpg";
+import blogSingleImg2 from "/public/images/blog/2.jpg";
+import blogSingleImg3 from "/public/images/blog/3.jpg";
+
+
+
+const blogs = [
+    {
+        id: '1',
+        title: 'Alumni meet',
+        catagory: 'Alumni',
+        slug: '',
+        description: 'It has survived not only five but also the leap intona electronic typesetting remaining unchanged.',
+        create_at: 'Sep 03, 2024',
+        name:"Alumni",
+        screens: blogImg1,
+        recent: recent1,
+        blogSingleImg: blogSingleImg1,
+        comment: '35',
+        day: '28',
+        month: 'AUGUST',
+        blClass: 'format-standard-image',
+        animation: '1200',
+    },
+    {
+        id: '2',
+        title: 'Admission Open ',
+        catagory: 'Admission',
+        slug: '',
+        description: 'It has survived not only five but also the leap intona electronic typesetting remaining unchanged.',
+        create_at: 'Sep 06, 2024',
+        name:"Admission",
+        screens: blogImg2,
+        recent: recent2,
+        blogSingleImg: blogSingleImg2,
+        comment: '35',
+        day: '28',
+        month: 'AUGUST',
+        blClass: 'format-standard-image',
+        animation: '1200',
+    },
+ 
+    {
+        id: '3',
+        title: 'Current opening',
+        catagory: 'opening',
+        slug: '',
+        description: 'It has survived not only five but also the leap intona electronic typesetting remaining unchanged.',
+        create_at: 'Sep 06, 2024',
+        name:"Recruitment",
+        screens: blogImg3,
+        recent: recent3,
+        blogSingleImg: blogSingleImg3,
+        comment: '35',
+        day: '28',
+        month: 'AUGUST',
+        blClass: 'format-standard-image',
+        animation: '1200',
+    },
+ 
+ 
+   
+
+];
+export default blogs;
