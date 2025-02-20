849d1fc17e9f0bda1a3affc76990d1be3156a2d3
ankitjnvynr <ankitbkana@outlook.com>
2025-02-20 16:29:25 +0530

    update

--- components/ServiceSection/ServiceSection.js
+++ components/ServiceSection/ServiceSection.js
@@ -1,12 +1,56 @@
 import React from "react";
-import Services from "./Services";
+
 import SectionTitle from "../SectionTitle/SectionTitle";
 import ServiceCard from "./ServiceCard"
 import ShapeOn from '/public/images/services/bg.jpeg'
 import Image from "next/image";
 
+import simg1 from '/public/images/services/icon-1.svg'
+import simg2 from '/public/images/services/icon-2.svg'
+import simg3 from '/public/images/services/icon-3.svg'
+import simg4 from '/public/images/services/icon-4.svg'
+import sSimg1 from '/public/images/service-single/1.jpg'
+import sSimg2 from '/public/images/service-single/2.jpg'
+import sSimg3 from '/public/images/service-single/3.jpg'
+import sSimg4 from '/public/images/service-single/4.jpg'
+
 
 const ServiceSection = (props) => {
+    const Services = [
+        {
+           Id: '01',
+           sImg: simg1,
+           sSImg: sSimg1,
+           title: 'MBA',
+           slug: '',
+           description: 'We are providing the best UI/UX design. That help you be professional.',
+        },
+        {
+           Id: '02',
+           sImg: simg2,
+           sSImg: sSimg2,
+           title: 'BBA',
+           slug: '',
+           description: 'We are providing the best UI/UX design. That help you be professional.',
+        },
+        {
+           Id: '03',
+           sImg: simg3,
+           sSImg: sSimg3,
+           title: 'BTECH',
+           slug: '',
+           description: 'We are providing the best UI/UX design. That help you be professional.',
+        },
+        {
+           Id: '04',
+           sImg: simg4,
+           sSImg: sSimg4,
+           title: 'MCA',
+           slug: '',
+           description: 'We are providing the best UI/UX design. That help you be professional.',
+        },
+      
+     ]
 
 
     return (
