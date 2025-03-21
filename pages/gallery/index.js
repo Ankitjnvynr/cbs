4f4e61856889a6b6bfb270277a6e2a4fba4a8eaa
ankitjnvynr <ankitbkana@outlook.com>
2025-03-21 11:48:06 +0530

    updated by amisha

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import PageTitle from "../../components/pagetitle/PageTitle";
-
+import Gallery from "../../components/Gallery/Gallery";
 import Scrollbar from "../../components/scrollbar/scrollbar";
 import Footer from "../../components/footer/Footer";
 import Logo from "/public/images/cbslogo.png";
@@ -33,7 +33,7 @@ function director() {
       <Navbar Logo={Logo} hclass={"wpo-site-header s1"} telephone={Phone} />
       <PageTitle pageTitle={"Gallery "} pagesub={"vision"} />
     
-
+<Gallery/>
 
 
       <Footer />
